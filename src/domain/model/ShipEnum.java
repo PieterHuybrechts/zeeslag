2e9f50a5ebbd482474387fd23b3b7e3d58415744
Brecht Decuyper <brecht.decuyper@student.kuleuven.be>
2016-11-25 16:02:34 +0100

    edit ShipEnum

---
+++ src/domain/model/ShipEnum.java
@@ -0,0 +1,25 @@
+package domain.model;
+
+public enum ShipEnum {
+	VLIEGDEKSCHIP ("Vliegdekschip", 5),
+	SLAGSCHIP ("Slagschip", 4),
+	ONDERZEEER ("Onderzeeër", 3),
+	TORPEDOBOOTJAGER ("Torpedobootjager", 3),
+	PATROUILLESCHIP ("Patrouilleschip", 2);
+	
+	private String name;
+	private int length;
+
+	private ShipEnum(String name, int length) {
+		this.name = name;
+		this.length = length;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getLength() {
+		return this.length;
+	}
+}
