314ad86de0498607bc1f68a944a7871dad854a7c
Brecht Decuyper <brecht.decuyper@student.kuleuven.be>
2016-11-25 16:10:55 +0100

    edit shipenum

--- src/domain/model/ShipEnum.java
+++ src/domain/model/ShipEnum.java
@@ -4,7 +4,8 @@ public enum ShipEnum {
 	VLIEGDEKSCHIP ("Vliegdekschip", 5),
 	SLAGSCHIP ("Slagschip", 4),
 	ONDERZEEER ("Onderzeeër", 3),
-	TORPEDOBOOTJAGER ("Torpedobootjager", 3);	
+	TORPEDOBOOTJAGER ("Torpedobootjager", 3),
+	PATROUILLESHIP ("Patrouilleschip", 2);
 
 	private String name;
 	private int length;
