6c7118ba2771c51efb96e6d01bbc6a866f4ede82
Pieter Huybrechts <pieter.huybrechts@student.ucll.be>
2016-12-02 16:42:39 +0100

    Strategyfactory + strategy

--- src/domain/model/Computer.java
+++ src/domain/model/Computer.java
@@ -1,12 +1,20 @@
 package domain.model;
 
 import domain.DomainException;
+import domain.PlaceShipFactory;
 import domain.model.lib.Position;
 
 public class Computer extends Player{
 
+	PlaceShipStrategy strat;
+	
 	public Computer(String name) throws DomainException{
 		super(name,new BSBoard());
+		strat = new PlaceShipFactory().getPlaceShipStrategy();
+	}
+	
+	private void createShips(){
+		
 	}
 
 	@Override
