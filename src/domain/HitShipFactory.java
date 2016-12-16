7c6134ddc67bf2bbcab3bc98d3ff044a4925a074
Pieter Huybrechts <pieter.huybrechts@student.ucll.be>
2016-12-16 16:17:58 +0100

    Conflict solution

---
+++ src/domain/HitShipFactory.java
@@ -0,0 +1,35 @@
+package domain;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import domain.model.HitShipStrategy;
+import domain.model.PlaceShipStrategy;
+
+public class HitShipFactory {
+	public HitShipStrategy getHitShipStrategy() throws DomainException {
+		Properties p = new Properties();
+
+		InputStream in;
+
+		try {
+			in = new FileInputStream(new File("resources\\StrategyProps.properties"));
+			p.load(in);
+		} catch (IOException e) {
+			throw new DomainException("Properties file not found");
+		}
+
+		String className = (String) p.get("strathit");
+		
+		
+		try {
+			Class<?> clazz = Class.forName(className);
+			return (HitShipStrategy) clazz.newInstance();
+		} catch (Exception e){
+			throw new DomainException("Strategy could not be initiated.");
+		}
+	}
+}
