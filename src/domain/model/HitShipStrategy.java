66fc763d303ec7fbf8998c44c4a8d8bafa4ba6e4
Thomas Vanzegbroeck <thomas.vanzegbroeck@hotmail.com>
2016-12-16 16:12:53 +0100

    Startegy en factory hitship

---
+++ src/domain/model/HitShipStrategy.java
@@ -0,0 +1,7 @@
+package domain.model;
+
+import domain.model.lib.Position;
+
+public interface HitShipStrategy {
+	public Position hitShip();
+}
---
+++ src/domain/model/RandomHitShipStrategy.java
@@ -0,0 +1,39 @@
+package domain.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import domain.model.lib.Position;
+
+public class RandomHitShipStrategy implements HitShipStrategy {
+	
+	private List<Position> nothittedPostion = new ArrayList<Position>();
+	private Random r = new Random();
+	public RandomHitShipStrategy(){
+		this.constructPositions();
+	}
+	
+	private void constructPositions(){
+		
+		for(int x = 0; x< 10;x++){
+			for(int y =0; y<10;y++){
+				Position pos = new Position(x,y);
+				this.nothittedPostion.add(pos);
+			}
+		}
+		
+	}
+
+
+	@Override
+	public Position hitShip() {
+		// TODO Auto-generated method stub
+		int i = r.nextInt(this.nothittedPostion.size());
+		Position pos = this.nothittedPostion.get(i);
+		this.nothittedPostion.remove(pos);
+		return pos;
+	
+	}
+	
+}
